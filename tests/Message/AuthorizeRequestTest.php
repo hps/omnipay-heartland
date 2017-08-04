21fbdcdf8db2f8ad721b07228d1f8dc949e17549
Senthil Kumar Muthusamy <sen.kumar2007@gmail.com>
2017-08-04 12:09:54 -0400

    Unit test added for auth payment using mock

--- tests/Message/AuthorizeRequestTest.php
+++ tests/Message/AuthorizeRequestTest.php
@@ -177,6 +177,32 @@ class AuthorizeRequestTest extends TestCase
         $this->assertInstanceOf('DOMElement', $data);
     }
     
+    /*
+    public function testSendSuccess()
+    {
+        $this->setMockHttpResponse('PurchaseSuccess.txt');
+        $response = $this->request->send();
+
+        $this->assertTrue($response->isSuccessful());
+        $this->assertFalse($response->isRedirect());
+        $this->assertSame('ch_1IU9gcUiNASROd', $response->getTransactionReference());
+        $this->assertSame('card_16n3EU2baUhq7QENSrstkoN0', $response->getCardReference());
+        $this->assertSame('req_8PDHeZazN2LwML', $response->getRequestId());
+        $this->assertNull($response->getMessage());
+    }
+
+    
+    public function testSendError()
+    {
+        $this->setMockHttpResponse('PurchaseFailure.txt');
+        $response = $this->request->send();
+
+        $this->assertFalse($response->isSuccessful());
+        $this->assertFalse($response->isRedirect());
+        $this->assertSame('ch_1IUAZQWFYrPooM', $response->getTransactionReference());
+        $this->assertNull($response->getCardReference());
+        $this->assertSame('Your card was declined', $response->getMessage());
+    }*/
     
     
     

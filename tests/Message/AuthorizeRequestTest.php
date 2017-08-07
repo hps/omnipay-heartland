5f1a501d28e085df3cd9f5c492e4ee81f97f0320
Senthil Kumar Muthusamy <sen.kumar2007@gmail.com>
2017-08-07 09:20:54 -0400

    curl error and gateway error tested

--- tests/Message/AuthorizeRequestTest.php
+++ tests/Message/AuthorizeRequestTest.php
@@ -204,6 +204,25 @@ class AuthorizeRequestTest extends TestCase
         $this->assertSame('1023522465', (string) $response->getTransactionReference());
     }
     
+    /**
+     * @expectedException \Omnipay\Common\Exception\InvalidResponseException
+     * @expectedExceptionMessage gateway_time-out
+     */
+    public function testSendGatewayError()
+    {
+        $this->setMockHttpResponse('AuthorizeFailureGatewayError.txt');
+        $response = $this->request->send();
+    }
+    
+    /**
+     * @expectedException \Omnipay\Common\Exception\InvalidResponseException
+     * @expectedExceptionMessage PHP-SDK cURL TLS 1.2 handshake failed. If you have any questions, please contact Specialty Products Team at 866.802.9753
+     */
+    public function testSendCurlError()
+    {
+        $this->setMockHttpResponse('AuthorizeFailureCurlError.txt');
+        $response = $this->request->send();
+    }
     
     
 }

2e09a3b45cedfe50b71194327b3530dd87aaa4fb
Senthil Kumar Muthusamy <sen.kumar2007@gmail.com>
2017-08-09 09:57:42 -0400

    negative test cases added

--- tests/GatewayIntegrationTest.php
+++ tests/GatewayIntegrationTest.php
@@ -102,11 +102,22 @@ class GatewayIntegrationTest extends TestCase {
         $this->assertTrue($response->isSuccessful(), 'Reversal should succeed');        
     }
     
-    public function testPurchaseWithInvalidToken() {
+    public function testPurchaseWithInvalidCardReference() {
         // Purchase
         $request = $this->gateway->purchase(array(
             'amount' => 10.00,
             'cardReference' => '123456'
+        ));
+        $response = $request->send();
+        $this->assertFalse($response->isSuccessful());
+        $this->assertSame('Invalid card data', $response->getMessage());    
+    }
+    
+    public function testPurchaseWithInvalidToken() {
+        // Purchase
+        $request = $this->gateway->purchase(array(
+            'amount' => 10.00,
+            'token' => '123456'
         ));
         $response = $request->send();
         $this->assertFalse($response->isSuccessful());
@@ -129,6 +140,34 @@ class GatewayIntegrationTest extends TestCase {
         ));
         $response = $request->send();
         $this->assertTrue($response->isSuccessful(), 'Fetch transaction details failed');        
+    }
+    
+    public function testAuthCaptureTwice() {
+        // Authorize
+        $request = $this->gateway->authorize(array(
+            'amount' => '42.42',
+            'card' => $this->getValidCard()
+        ));
+        $response = $request->send();
+        $this->assertTrue($response->isSuccessful(), 'Authorization should succeed');
+        $transactionRef = $response->getTransactionReference();
+
+        // Capture
+        $request = $this->gateway->capture(array(
+            'amount' => '42.42',
+            'transactionReference' => $transactionRef
+        ));
+        $response = $request->send();
+        $this->assertTrue($response->isSuccessful(), 'Capture should succeed');
+        
+        // Capture again
+        $request = $this->gateway->capture(array(
+            'amount' => '42.42',
+            'transactionReference' => $transactionRef
+        ));
+        $response = $request->send();
+        $this->assertFalse($response->isSuccessful());
+        $this->assertSame('Transaction rejected because the referenced original transaction is invalid. Subject \''.$transactionRef.'\'.  Original transaction is already part of a batch.', $response->getMessage());         
     }
 
 }
